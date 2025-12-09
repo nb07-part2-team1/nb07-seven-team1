95d48f3fd2e5ff1718e4f51ae5e235811375d161
서석규 <ssk7594@gmail.com>
2025-12-09 06:20:44 +0000

    feat : workoutRecord 도메인 모델 및 CRUD 설정

---
+++ src/modeules/groups/groups-workout-record.model.js
@@ -0,0 +1,47 @@
+//../modeules/groups/groups-workout-record.model.js
+
+export class WorkoutRecord {
+  constructor(
+    id,
+    nickname,
+    workoutType,
+    description,
+    time,
+    distance,
+    images,
+    createdAt
+  ) {
+    this.id = id;
+    this.nickname = nickname;
+    this.workoutType = workoutType;
+    this.description = description;
+    this.time = time;
+    this.distance = distance;
+    this.images = images;
+    this.createdAt = createdAt;
+  }
+
+  static fromEntity(entity) {
+    const info = {
+      id: entity.id.toString(),
+      nickname: entity.user.name,
+      workoutType: entity.category,
+      description: entity.description,
+      time: entity.duration_seconds,
+      distance: entity.distance,
+      images: entity.images || [],
+      createdAt: entity.created_at,
+    };
+
+    return new WorkoutRecord(
+      info.id,
+      info.nickname,
+      info.workoutType,
+      info.description,
+      info.time,
+      info.distance,
+      info.images,
+      info.createdAt
+    );
+  }
+}
