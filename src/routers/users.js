2d6e1f6bc1b0410f6aae24aa03af54f7f18537a7
hyl <laatu77@gmail.com>
2025-12-11 06:56:03 +0000

    feat: controller, service, mapper 기능별 세분화

--- src/routers/users.js
+++ src/routers/users.js
@@ -3,7 +3,7 @@ import * as usersController from "../modeules/users/index.js";
 
 const router = express.Router();
 
-router.post("/:groupId/users", usersController.createUser);
-router.delete("/:groupId/users", usersController.deleteUser);
+router.post("/:groupId/participants", usersController.createUser);
+router.delete("/:groupId/participants", usersController.deleteUser);
 
 export default router;
