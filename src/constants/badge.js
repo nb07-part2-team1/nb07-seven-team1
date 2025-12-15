b11ad887ac59cbbc85d6e6e228966ecf733bc47b
hiiisiii <8243.hk@gmail.com>
2025-12-15 08:06:14 +0000

    refactor: extract group badge constants

---
+++ src/constants/badge.js
@@ -0,0 +1,11 @@
+export const badgeContent = {
+  participantsBadge10: "참여자 10명 이상",
+  likesBadge100: "추천수 100 이상",
+  workoutsBadge100: "운동 기록 100개 이상",
+};
+
+export const badgeThreshold = {
+  participantsBadge10: 10,
+  likesBadge100: 100,
+  workoutsBadge100: 100,
+};
