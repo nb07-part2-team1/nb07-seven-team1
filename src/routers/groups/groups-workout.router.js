ca259b76120cdff6666e66c65e640cce4b23a812
서석규 <ssk7594@gmail.com>
2025-12-16 01:28:48 +0000

    feat: crud 구현

--- src/routers/groups/groups-workout.router.js
+++ src/routers/groups/groups-workout.router.js
@@ -1,6 +1,92 @@
 import express from "express";
-import * as groupWorkoutController from "../../modeules/groups/groups-workout.js";
+import { validateWorkoutRecord } from "../../middlewares/workout.middleware.js";
+import {
+  createRecord,
+  getRecords,
+  getRecordDetail,
+  updateRecord,
+  deleteRecord,
+} from "../../modeules/groups/groups-workout.js";
 
 const router = express.Router();
+
+//post
+router.post("/records", validateWorkoutRecord, async (req, res, next) => {
+  try {
+    const newRecord = await createRecord(req.body);
+
+    return res.status(201).json({
+      message: "운동 기록이 성공적으로 등록되었습니다.",
+      record: newRecord,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+//get
+router.get("/records", async (req, res, next) => {
+  try {
+    const records = await getRecords(req.query);
+
+    return res.status(200).json(records);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//get 상세
+router.get("/records/:recordId", async (req, res, next) => {
+  try {
+    const { recordId } = req.params;
+    const recordDetail = await getRecordDetail(recordId);
+
+    return res.status(200).json(recordDetail);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//patch
+router.patch(
+  "/records/:recordId",
+  validateWorkoutRecord,
+  async (req, res, next) => {
+    try {
+      const { recordId } = req.params;
+      const { authorNickname, authorPassword, ...updateData } = req.body;
+
+      const updatedRecord = await updateRecord(
+        recordId,
+        updateData,
+        authorNickname,
+        authorPassword
+      );
+
+      return res.status(200).json({
+        message: "운동 기록이 성공적으로 수정되었습니다.",
+        record: updatedRecord,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+//delete
+router.delete("/records/:recordId", async (req, res, next) => {
+  try {
+    const { recordId } = req.params;
+    const { authorNickname, authorPassword } = req.body;
+
+    const result = await deleteRecord(recordId, authorNickname, authorPassword);
+
+    return res.status(200).json({
+      message: "운동 기록이 성공적으로 삭제되었습니다.",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
