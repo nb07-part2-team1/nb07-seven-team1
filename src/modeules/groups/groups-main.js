e29cf8269032b59dffdb272daf6e3db1655cd65b
dlckdgh <dlckdgh0523@gmail.com>
2025-12-17 06:07:49 +0000

    feat:그룹 생성,목록가져오기,상세정보가져오기추가

---
+++ src/modeules/groups/groups-main.js
@@ -0,0 +1,139 @@
+import { UnregistereGroup } from "../../entities/group.js";
+import { prisma } from "../../../prisma/prisma.js";
+import { UnregisteredUser, User } from "../../entities/user.js";
+import { error } from "node:console";
+
+const formatGroupResponse = (group) => {
+  const owner = group.users.find((user) => group.owner.user_id === user.id);
+  const participants = group.users.map((user) => ({
+    id: Number(user.id),
+    nickname: user.name,
+    createdAt: user.created_at.getTime(),
+    updatedAt: user.updated_at.getTime(),
+  }));
+  const badges = group.badges.map((badge) => badge.name);
+
+  return {
+    id: Number(group.id),
+    name: group.name,
+    description: group.description,
+    photoUrl: group.image ?? null,
+    goalRep: group.goal_reps,
+    discordWebhookUrl: group.discord_web_url,
+    discordInviteUrl: group.discord_server_url,
+    likeCount: group.like_count,
+    tags: group.tags ?? [],
+    owner: {
+      id: Number(owner.id),
+      nickname: owner.name,
+      createdAt: owner.created_at.getTime(),
+      updatedAt: owner.updated_at.getTime(),
+    },
+    participants,
+    createdAt: group.created_at.getTime(),
+    updatedAt: group.updated_at.getTime(),
+    badges,
+  };
+};
+export const createGroup = async (req, res, next) => {
+  try {
+    const createGroupInfo = UnregistereGroup.formInfo(req.body);
+    const createdGroup = await prisma.group.create({
+      data: { ...createGroupInfo, like_count: 0 },
+    });
+
+    const createOwnerInfo = UnregisteredUser.formInfo({
+      name: req.body.ownerNickname,
+      password: req.body.ownerPassword,
+      groupId: createdGroup.id,
+    });
+
+    const inputUser = await prisma.user.create({
+      data: createOwnerInfo,
+    });
+    await prisma.owner.create({
+      data: {
+        user_id: inputUser.id,
+        group_id: createdGroup.id,
+      },
+    });
+    const newGroupInfo = await prisma.group.findUnique({
+      where: { id: createdGroup.id },
+      include: {
+        owner: true,
+        users: true,
+        badges: true,
+      },
+    });
+    const result = formatGroupResponse(newGroupInfo);
+
+    res.status(201).json(result);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getGroup = async (req, res, next) => {
+  try {
+    const groupId = req.params.groupId;
+
+    const newGroupInfo = await prisma.group.findUnique({
+      where: { id: groupId },
+      include: {
+        owner: true,
+        users: true,
+        badges: true,
+      },
+    });
+
+    const result = formatGroupResponse(newGroupInfo);
+    res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+  }
+};
+const getOrderBy = (order, direction) => {
+  switch (order) {
+    case "likeCount":
+      return { like_count: direction };
+
+    case "participantCount":
+      return {
+        users: {
+          _count: direction,
+        },
+      };
+
+    case "createdAt":
+    default:
+      return { created_at: direction };
+  }
+};
+
+export const getGroups = async (req, res) => {
+  try {
+    const { page, limit, order, orderBy, search } = req.query;
+    const newOrderBy = getOrderBy(orderBy, order);
+    const [groups, groupCount] = await Promise.all([
+      prisma.group.findMany({
+        ...(!search && {
+          where: { name: { contains: search, mode: "insensitive" } },
+        }),
+        skip: (parseInt(page) - 1) * parseInt(limit),
+        take: parseInt(limit),
+        orderBy: newOrderBy,
+        include: {
+          owner: true,
+          users: true,
+          badges: true,
+        },
+      }),
+      prisma.group.count(),
+    ]);
+    const result = groups.map((group) => formatGroupResponse(group));
+
+    res.status(200).json({ data: result, total: groupCount });
+  } catch (error) {
+    console.log(error);
+  }
+};
