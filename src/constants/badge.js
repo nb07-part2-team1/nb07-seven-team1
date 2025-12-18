c30596e93ce9250364d1a0e8877d555f96e44a97
hiiisiii <8243.hk@gmail.com>
2025-12-18 18:18:51 +0900

    Rename badge constants for consistency

--- src/constants/badge.js
+++ src/constants/badge.js
@@ -1,11 +1,11 @@
 export const BADGE_CONTENT = {
-  PARTICIPANTS_10: "참여자 10명 이상",
-  LIKES_100: "추천수 100 이상",
-  WORKOUTS_100: "운동 기록 100개 이상",
+  PARTICIPATION_10: "참여자 10명 이상",
+  LIKE_100: "추천수 100 이상",
+  RECORD_100: "운동 기록 100개 이상",
 };
 
 export const BADGE_THRESHOLD = {
-  PARTICIPANTS_10: 10,
-  LIKES_100: 100,
-  WORKOUTS_100: 100,
+  PARTICIPATION_10: 10,
+  LIKE_100: 100,
+  RECORD_100: 100,
 };
