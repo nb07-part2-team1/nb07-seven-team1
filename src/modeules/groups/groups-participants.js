08846567b9ca7eb6e08fe4f5ef53914f727899c0
hiiisiii <8243.hk@gmail.com>
2025-12-18 02:19:18 +0000

    feat: 그룹 참여/참여취소

---
+++ src/modeules/groups/groups-participants.js
@@ -0,0 +1,88 @@
+import { prisma } from "../../../prisma/prisma.js";
+import { Group } from "../../entities/group.js";
+import { User } from "../../entities/user.js";
+import { participantsBadge } from "./groups-badge.js";
+import { checkMember, checkNickname } from "../../utils/auth.js";
+import { NotFoundError, BadRequestError } from "../../errors/customError.js";
+
+export const joinGroup = async (req, res, next) => {
+  try {
+    const groupId = BigInt(req.params.groupId);
+    const { nickname, password } = req.body;
+
+    const group = await prisma.group.findUnique({
+      where: { id: groupId },
+    });
+
+    if (!group) {
+      throw new NotFoundError("존재하지 않는 그룹입니다.");
+    }
+
+    await checkNickname(groupId, nickname);
+
+    await prisma.user.create({
+      data: {
+        name: nickname,
+        password,
+        group_id: groupId,
+      },
+    });
+
+    await participantsBadge(groupId);
+
+    const updatedGroup = await prisma.group.findUnique({
+      where: { id: groupId },
+      include: {
+        owner: true,
+        users: true,
+        badges: true,
+      },
+    });
+
+    const groupEntity = Group.formEntity(updatedGroup);
+
+    return res.status(200).json({
+      ...groupEntity,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const leaveGroup = async (req, res, next) => {
+  try {
+    const groupId = BigInt(req.params.groupId);
+    const { nickname, password } = req.body;
+
+    const group = await prisma.group.findUnique({
+      where: { id: groupId },
+      select: { id: true },
+    });
+
+    if (!group) {
+      throw new NotFoundError("존재하지 않는 그룹입니다.");
+    }
+
+    const user = await checkMember(groupId, nickname, password);
+
+    // 오너 탈퇴 불가
+    const owner = await prisma.owner.findUnique({
+      where: { group_id: groupId },
+      select: { user_id: true },
+    });
+
+    if (owner?.user_id === user.id) {
+      throw new BadRequestError(
+        "방장은 그룹을 탈퇴할 수 없습니다. 그룹 삭제를 이용해주세요."
+      );
+    }
+
+    await prisma.user.delete({
+      where: { id: user.id },
+    });
+
+    return res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
+};
