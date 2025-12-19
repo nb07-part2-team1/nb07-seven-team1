3c67a888e5d7501ab997e23ed4003b10378b2dc3
hyl <laatu77@gmail.com>
2025-12-19 02:51:08 +0000

    refactor: 함수 분리

--- src/modules/groups/groups-participants.js
+++ src/modules/groups/groups-participants.js
@@ -1,36 +1,82 @@
 import { prisma } from "../../../prisma/prisma.js";
 import { Group } from "../../entities/group.js";
-import { User } from "../../entities/user.js";
+import { UnregisteredUser, User } from "../../entities/user.js";
 import { participantsBadge } from "./groups-badge.js";
 import { checkMember, checkNickname } from "../../utils/auth.js";
-import { NotFoundError } from "../../errors/customError.js";
+import { BadRequestError, NotFoundError } from "../../errors/customError.js";
+
+//그룹 체크
+const existGroup = async (group_id) => {
+  const getGroupId = await prisma.group.findUnique({
+    where: { id: group_id },
+    select: { id: true },
+  });
+
+  if (!getGroupId) {
+    throw new NotFoundError("존재하지 않는 그룹입니다.");
+  }
+};
+
+//닉네임 생성
+const createNickname = async ({ group_id, nickname, password }) => {
+  const user = await prisma.user.create({
+    data: {
+      group_id,
+      name: nickname,
+      password,
+    },
+  });
+
+  return user;
+};
+
+//오너 체크
+const checkOwner = async ({ group_id, user_id }) => {
+  const owner = await prisma.owner.findUnique({
+    where: { group_id, user_id },
+  });
+
+  if (owner) {
+    throw new BadRequestError({
+      message: "방장은 그룹을 탈퇴할 수 없습니다",
+      path: "owner",
+    });
+  }
+};
 
 export const joinGroup = async (req, res, next) => {
   try {
     const groupId = BigInt(req.params.groupId);
     const { nickname, password } = req.body;
 
-    const group = await prisma.group.findUnique({
-      where: { id: groupId },
+    // 존재하는 그룹인지 체크
+    await existGroup(groupId);
+
+    // 닉네임 중복 체크
+    await checkNickname({ groupId, nickname });
+
+    // 닉네임&비밀번호 검증
+    const beforeVerificationUser = UnregisteredUser.formInfo({
+      name: nickname,
+      password,
+      groupId,
     });
 
-    if (!group) {
-      throw new NotFoundError("존재하지 않는 그룹입니다.");
-    }
-
-    await checkNickname(groupId, nickname);
-
-    await prisma.user.create({
-      data: {
-        name: nickname,
-        password,
-        group_id: groupId,
-      },
+    // 닉네임 생성
+    const createUser = await createNickname({
+      group_id: beforeVerificationUser.group_id,
+      nickname: beforeVerificationUser.name,
+      password: beforeVerificationUser.password,
     });
 
+    // DB 데이터 검증
+    User.formEntity(createUser);
+
+    // 그룹의 참여자 수 조회 (참여자 10명 이상이면 뱃지 부여)
     await participantsBadge(groupId);
 
-    const updatedGroup = await prisma.group.findUnique({
+    // owner, users, badges 정보 포함해서 group 조회
+    const groupWithRelations = await prisma.group.findUnique({
       where: { id: groupId },
       include: {
         owner: true,
@@ -39,7 +85,7 @@ export const joinGroup = async (req, res, next) => {
       },
     });
 
-    const groupEntity = Group.formEntity(updatedGroup);
+    const groupEntity = Group.formEntity(groupWithRelations);
 
     return res.status(200).json({
       ...groupEntity,
@@ -49,31 +95,22 @@ export const joinGroup = async (req, res, next) => {
   }
 };
 
-
 export const leaveGroup = async (req, res, next) => {
   try {
     const groupId = BigInt(req.params.groupId);
     const { nickname, password } = req.body;
 
-    const group = await prisma.group.findUnique({
-      where: { id: groupId },
-      select: { id: true },
+    // 존재하는 그룹인지 체크
+    existGroup(groupId);
+
+    // 그룹의 참가 멤버인지 체크
+    const user = await checkMember({ groupId, nickname, password });
+
+    // 방장 체크 (방장은 탈퇴할 수 없음)
+    await checkOwner({
+      group_id: user.group_id,
+      user_id: user.id,
     });
-
-    if (!group) {
-      throw new NotFoundError("존재하지 않는 그룹입니다.");
-    }
-
-    const user = await checkMember(groupId, nickname, password);
-
-    // 오너 탈퇴 불가
-    const owner = await prisma.owner.findUnique({
-      where: { group_id: groupId },
-      select: { user_id: true },
-    });
-    if (owner?.user_id === user.id) {
-      throw new BadRequestError("방장은 그룹을 탈퇴할 수 없습니다.");
-    }
 
     await prisma.user.delete({
       where: { id: user.id },
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -5,24 +5,36 @@ import {
   ConflictError,
 } from "../errors/customError.js";
 
-export const checkMember = async (groupIdBigInt, name, password) => {
+export const checkMember = async ({ groupId, nickname, password }) => {
   const user = await prisma.user.findFirst({
-    where: { group_id: groupIdBigInt, name },
-    select: { id: true, name: true, password: true },
+    where: { groupId, nickname },
+    select: { id: true, nickname: true, password: true },
   });
 
-  if (!user) throw new NotFoundError("해당 닉네임을 찾을 수 없습니다.");
-  if (user.password !== password)
-    throw new UnauthorizedError("비밀번호가 일치하지 않습니다.");
+  if (!user) {
+    throw new NotFoundError({
+      message: "해당 닉네임을 찾을 수 없습니다",
+      path: "nickname",
+    });
+  }
+  if (user.name === nickname && user.password !== password) {
+    throw new UnauthorizedError({
+      message: "비밀번호가 일치하지 않습니다",
+      path: "password",
+    });
+  }
 
   return user;
 };
 
-export const checkNickname = async (groupIdBigInt, name) => {
-  const exists = await prisma.user.findFirst({
-    where: { group_id: groupIdBigInt, name },
-    select: { id: true },
+export const checkNickname = async ({ groupId, nickname }) => {
+  const existsName = await prisma.user.findFirst({
+    where: { groupId, nickname },
   });
 
-  if (exists) throw new ConflictError("이미 사용 중인 닉네임입니다.");
+  if (existsName)
+    throw new ConflictError({
+      message: "이미 사용 중인 닉네임입니다",
+      path: "nickname",
+    });
 };
