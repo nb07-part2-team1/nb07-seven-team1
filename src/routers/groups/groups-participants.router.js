7db88a151643e8bfe40a5caa5916376e457d25bc
sinyeongseok <sinyeongseok99@gmail.com>
2025-12-21 06:18:50 +0000

    refactor: 그룹 참여 api 메소트 형식 -> 컨트롤러 클래스 형식으로 변경

--- src/routers/groups/groups-participants.router.js
+++ src/routers/groups/groups-participants.router.js
@@ -1,12 +1,12 @@
 import express from "express";
-import * as groupParticipantsController from "../../modules/groups/groups-participants.js";
+import GroupParticipantsController from "../../modules/groups/groups-participants.controller.js";
 
 const router = express.Router();
 
 // POST /groups/{groupId}/participants
-router.post("/:groupId/participants", groupParticipantsController.joinGroup);
+router.post("/:groupId/participants", GroupParticipantsController.join);
 
 // DELETE /groups/{groupId}/participants
-router.delete("/:groupId/participants", groupParticipantsController.leaveGroup);
+router.delete("/:groupId/participants", GroupParticipantsController.leave);
 
 export default router;
