82df49eb106aa53928040d402a12615f65734f41
sinyeongseok <sinyeongseok99@gmail.com>
2025-12-21 13:44:06 +0000

    feat: 필수 검증 및 타입 검증 미들웨어 연결

--- src/routers/groups/groups-main.router.js
+++ src/routers/groups/groups-main.router.js
@@ -1,10 +1,45 @@
 import express from "express";
-import * as groupMainController from "../../modules/groups/groups-main.js";
+import GroupMainController from "../../modules/groups/groups-main.controller.js";
+import { validateRequest } from "../../middlewares/validateRequest.middleware.js";
 
 const router = express.Router();
-router.post("/", groupMainController.createGroup);
-router.get("/", groupMainController.getGroups);
-router.get("/:groupId", groupMainController.getGroup);
-router.patch("/:groupId", groupMainController.patchGroup);
-router.delete("/:groupId", groupMainController.deleteGroup);
+router.post(
+  "/",
+  validateRequest(
+    {
+      name: { type: "string", required: true },
+      description: { type: "string", required: true },
+      photoUrl: { type: "string" },
+      goalRep: { type: "number", required: true },
+      discordWebhookUrl: { type: "string", required: true },
+      discordInviteUrl: { type: "string", required: true },
+      tags: { type: "array" },
+      ownerNickname: { type: "string", required: true },
+      ownerPassword: { type: "string", required: true },
+    },
+    "body"
+  ),
+  GroupMainController.createGroup
+);
+router.get("/", GroupMainController.getGroups);
+router.get("/:groupId", GroupMainController.getGroup);
+router.patch(
+  "/:groupId",
+  validateRequest(
+    {
+      name: { type: "string", required: true },
+      description: { type: "string", required: true },
+      photoUrl: { type: "string" },
+      goalRep: { type: "number", required: true },
+      discordWebhookUrl: { type: "string", required: true },
+      discordInviteUrl: { type: "string", required: true },
+      tags: { type: "array" },
+      ownerNickname: { type: "string", required: true },
+      ownerPassword: { type: "string", required: true },
+    },
+    "body"
+  ),
+  GroupMainController.updateGroup
+);
+router.delete("/:groupId", GroupMainController.deleteGroup);
 export default router;
