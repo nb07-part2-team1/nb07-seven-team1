cd662bf2ea12ddafa29e9a5d39f87f108cc2e767
sinyeongseok <sinyeongseok99@gmail.com>
2025-12-21 06:05:21 +0000

    refactor: 이미지 업로드 api 메소드 형식 -> 컨트롤러 클래스 형식으로 변경

--- src/routers/images.js
+++ src/routers/images.js
@@ -1,9 +1,9 @@
 import express from "express";
-import * as imageController from "../modules/images/index.js";
+import UploadController from "../modules/images/image.controller.js";
 import { upload } from "../config/multer.config.js";
 
 const router = express.Router();
 
-router.post("/", upload.array("files", 10), imageController.uploadImages);
+router.post("/", upload.array("files", 10), UploadController.uploadImages);
 
 export default router;
